ce0fc9062875c02cd8423b6783555045d7259fa8
ashraful-islam-siam <ashrafulislam.dev1@gmail.com>
2022-01-17 10:15:01 +0600

    1 file modified, 1 added

---
+++ src/components/Inputs/Inputs.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+
+class Inputs extends Component {
+  state = {
+    name: '',
+    country: '',
+    bio: '',
+    birthDay: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  render() {
+    const { name, country, bio, birthDay } = this.state;
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          name="name"
+          value={name}
+          onChange={this.handleChange}
+        />{' '}
+        <br /> <br />
+        <select name="country" value={country} onChange={this.handleChange}>
+          <option>Select Country</option>
+          <option value="Bangladesh">Bangladesh</option>
+          <option value="India">India</option>
+          <option value="Indonesia">Indonesia</option>
+          <option value="China">China</option>
+        </select>
+        <br />
+        <textarea
+          onChange={this.handleChange}
+          name="bio"
+          placeholder="Tell me About yourself"
+          value={bio}
+        ></textarea>{' '}
+        <br />
+        <input
+          type="date"
+          name="birthDay"
+          value={birthDay}
+          onChange={this.handleChange}
+        />{' '}
+        <br />
+        <input type="radio" name="gender" value="Male" />
+        Male
+        <input type="radio" name="gender" value="Female" />
+        Female
+        <input type="radio" name="gender" value="Others" />
+        Others
+        <br />
+        <button onClick={() => console.log(this.state)}>Show Data</button>
+      </div>
+    );
+  }
+}
+
+export default Inputs;
